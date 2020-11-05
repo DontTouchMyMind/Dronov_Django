547bfffb34de62727b38b68361ad4194d68fc68d
DontTouchMyMind <tobigface@gmail.com>
2020-11-05 15:40:17 +0200

    Fixed error.

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -4,7 +4,7 @@ from .models import Bb
 
 # Register your models here.
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'publised')
+    list_display = ('title', 'content', 'price', 'published')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content',)
 
